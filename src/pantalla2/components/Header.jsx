d9df68ca100616af0e2ab9e01065397198b7452c
yurikt13 <bangtantaejunk@gmail.com>
2020-09-14 23:11:59 -0500

    Se crearon algunas pantallas con sus componentes

---
+++ src/pantalla2/components/Header.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+
+class Header extends Component {
+    constructor(props) {
+        super (props);
+        this.state = {  }
+    }
+    render() {
+        return ( 
+            <h1>Holaaa</h1>
+        );
+    }
+}
+export default Header;
